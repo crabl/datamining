273fd75b0ea2df2272dd805e73407642adf36998
Chris Rabl <rabl.christopher@gmail.com>
2013-10-10 14:35:25 -0600

    TRN is now implemented mostly correctly

--- trn.py
+++ trn.py
@@ -28,49 +28,49 @@ def trn(data_set, max_iterations, codebook_size, epsilon_i, epsilon_f, lambda_i,
                 (0, 30),
                 (0, 30)]))
 
-    for __data_point__ in range(len(data_set)):
+    for t in range(max_iterations):
         # Progress bar
-        amtDone = float(__data_point__) / len(data_set) * 100
+        amtDone = float(t) / max_iterations * 100
         sys.stdout.write("\r%.2f%%" %amtDone)
         sys.stdout.flush()
 
-        random_data_point = data_set[random.randint(0, len(data_set) - 1)] # choose a random data point
-
-        for t in range(max_iterations):
-            codebooks_near_point = np.zeros(codebook_size)
-            
-            # for each w[i] find the number of w[j] such that || x-w[j] || < || x - w[i] ||
-            # array of all x - w[i]
-            # array of all || x - w[i] ||
-            distances = []
-
-            for i in range(codebook_size):
-                distances.append(np.linalg.norm(random_data_point - codebook[i]))
+        # Select random data point
+        random_data_point = data_set[random.randint(0, len(data_set)-1)]
         
-            lambda_val = lambda_i * ((lambda_f / lambda_i) ** (float(t) / max_iterations));
-            epsilon = epsilon_i * ((epsilon_f / epsilon_i) ** (float(t) / max_iterations));
-            for i in range(codebook_size):
-                codebooks_near_point[i] = np.sum(distances < distances[i])
-                # update the codebook vectors according to the neural gas algorithm
-                #for i in range(0, codebook_size):
-                codebook[i] = codebook[i] + epsilon * np.exp(-1 * codebooks_near_point[i] / lambda_val) * (random_data_point - codebook[i])
+        codebooks_near_point = np.zeros(codebook_size)
+            
+        # for each w[i] find the number of w[j] such that || x-w[j] || < || x - w[i] ||
+        # array of all x - w[i]
+        # array of all || x - w[i] ||
+        distances = []
 
-            # find closest two codebook indices <- SOMETHING WRONG HERE
-            smallest1, smallest2 = heapq.nsmallest(2, [(v, i) for (i, v) in enumerate(distances)])
-            index_smallest1, index_smallest2 = smallest1[1], smallest2[1]
-            # create connection and refresh existing connection
-            connections[index_smallest1, index_smallest2] = [1, 0]
-            connections[index_smallest2, index_smallest1] = [1, 0]
+        for i in range(codebook_size):
+            distances.append(np.linalg.norm(random_data_point - codebook[i]))
+        
+        lambda_val = lambda_i * ((lambda_f / lambda_i) ** (float(t) / max_iterations));
+        epsilon = epsilon_i * ((epsilon_f / epsilon_i) ** (float(t) / max_iterations));
+        for i in range(codebook_size):
+            codebooks_near_point[i] = np.sum(distances < distances[i])
+            # update the codebook vectors according to the neural gas algorithm
+            #for i in range(0, codebook_size):
+            codebook[i] = codebook[i] + epsilon * np.exp(-1 * codebooks_near_point[i] / lambda_val) * (random_data_point - codebook[i])
+
+        # find closest two codebook indices <- SOMETHING WRONG HERE
+        smallest1, smallest2 = heapq.nsmallest(2, [(v, i) for (i, v) in enumerate(distances)])
+        index_smallest1, index_smallest2 = smallest1[1], smallest2[1]
+        # create connection and refresh existing connection
+        connections[index_smallest1, index_smallest2] = [1, 0]
+        connections[index_smallest2, index_smallest1] = [1, 0]
             
             
-            # age all connections <- THIS IS TERRIBLE
-            max_age = T_i * ((T_f / T_i) ** (float(t) / max_iterations));
-            for row in range(len(connections)):
-                for col in range(len(connections[row])):
-                    if connections[row][col] != None:
-                        connections[row][col][1] += 1
-                        if connections[row][col][1] > max_age:
-                            connections[row][col] = None
+        # age all connections <- THIS IS TERRIBLE
+        max_age = T_i * ((T_f / T_i) ** (float(t) / max_iterations));
+        for row in range(len(connections)):
+            for col in range(len(connections[row])):
+                if connections[row][col] != None:
+                    connections[row][col][1] += 1
+                    if connections[row][col][1] > max_age:
+                        connections[row][col] = None
 
     return codebook, connections
 
@@ -106,21 +106,21 @@ def main():
     num_points = len(dataset)
     import time
     t0 = time.time()
-    max_iter = 100
     epsilon_i = 0.3
     epsilon_f = 0.05
     lambda_i = 0.2 * num_points
     lambda_f = 0.01
     T_i = 0.1 * num_points
     T_f = 0.5 * num_points
-    codebook_size = 20
+    codebook_size = 200
+    max_iter = 200 * codebook_size
     print "Constructing network on", len(dataset), "data points..."
     codebook, connections = trn(dataset, max_iter, codebook_size, epsilon_i, epsilon_f, lambda_i, lambda_f, T_i, T_f)
     print "TRN Runtime:", time.time() - t0
 
     print "\nCodebook:"
     for item in codebook:
-        print item[0],",", item[1]
+        print item[0],",", item[1],",",item[2]
 
     print "\nConnections:"
 
