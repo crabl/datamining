bd5afd107cfeec407b5e02fc481d292775a240a2
Chris Rabl <rabl.christopher@gmail.com>
2013-11-06 18:44:17 -0700

    Parsing json data message

--- static/websocket_test.js
+++ static/websocket_test.js
@@ -1,6 +1,13 @@
 function getTRNData() {
   var sock = new WebSocket("ws://"+document.domain+"/trn");
-  sock.onmessage = function(msg) { console.log(msg.data); };
+  sock.onmessage = function(msg) {
+    var message = JSON.parse(msg.data);
+    if(message.nodes) {
+      console.log("TRN DATA DETECTED")
+    }
+    console.log(msg.data);
+
+  };
   if(sock.readyState == 1) {
     sock.send('some data');
   } else {
