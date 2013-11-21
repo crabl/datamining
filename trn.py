679fa7c144d9955449d8fd8e7911138d456596d6
Chris Rabl <rabl.christopher@gmail.com>
2013-11-21 13:22:33 -0700

    Removed some legacy visualization code

--- trn.py
+++ trn.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# TRNMap Algorithm by Vass-Fogarassy, Ábonyi, et al. [2008]
+# Implemented in Numpy by Christopher Rabl
+
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -38,52 +41,63 @@ def connect_graph(codebook, connections):
 
     num_subgraphs = len(np.unique(candidate_edges.argmin(0)))
     print "    Remaining subgraphs: ",
+
+    # While we have unconnected subgraphs
     while num_subgraphs > 1:
         print num_subgraphs, 
-        shortest_new_edge = np.nanmin(candidate_edges) # Find the smallest entry
-        new_edge_indices = np.where(candidate_edges==shortest_new_edge) # Find the indices of that entry
+
+        # Find the smallest entry
+        shortest_new_edge = np.nanmin(candidate_edges) 
+        # Find the indices of that entry
+        new_edge_indices = np.where(candidate_edges==shortest_new_edge) 
 
         if len(new_edge_indices[0]) > 1:
             new_edge_indices = new_edge_indices[0]
 
-        connections[new_edge_indices[0], new_edge_indices[1]] = 1 # Add the new edge to the connection graph
+        # Add the new edge to the connection graph
+        connections[new_edge_indices[0], new_edge_indices[1]] = 1 
         connections[new_edge_indices[1], new_edge_indices[0]] = 1
 
         dijkstra_distances = geodesic_distance(codebook, connections)
-        np.putmask(dijkstra_distances, dijkstra_distances==np.inf, 0) # replace all infinite edges with 0
+        # Replace all infinite edges from Dijkstra's output with 0 mask
+        np.putmask(dijkstra_distances, dijkstra_distances==np.inf, 0)
 
+        # Recompute candidate edge set now that we've hopefully added a connection
         distances = dist.squareform(dist.pdist(codebook, 'euclidean'))
-
         candidate_edges = distances - dijkstra_distances
         np.putmask(candidate_edges, candidate_edges <= 0, np.nan)
-
         num_subgraphs = len(np.unique(candidate_edges.argmin(0)))
+
     print "Done!",
 
     return connections
 
 def MDS(codebook, dimensions):
     num_points = len(codebook)
-    P = dist.squareform(dist.pdist(codebook, 'euclidean'))**2 # matrix of SQUARED distances (really this is P^2)
+
+    # matrix of SQUARED distances (really this is P^2)
+    P = dist.squareform(dist.pdist(codebook, 'euclidean'))**2 
     I = np.identity(num_points)
     ONE = np.ones((num_points, num_points))
-    J = I - (1./num_points) * ONE
+    J = I - (1./num_points) * ONE # Centering matrix for MDS
     B = -0.5 * J * P * J
+
     # Calculate the d largest eigenvalues (L) and corresponding eigenvectors (E)
     (L, E) = largest_eigh(B, eigvals=(num_points - dimensions, num_points - 1))
-    L = np.sqrt(np.flipud(L)) # Sort from highest to lowest and square root
-    E = np.fliplr(E) # sort from largest to smallest positive eigenvectors
+
+    # Sort from highest to lowest and square root
+    L = np.sqrt(np.flipud(L)) 
+
+    # Sort from largest to smallest positive eigenvectors
+    E = np.fliplr(E) 
+
     return -1*(E*L) # don't know why we need the -1...
 
 def TRN(data_set, max_iterations, codebook_size, epsilon_i, epsilon_f, lambda_i, lambda_f, T_i, T_f):
     connections = np.zeros((codebook_size, codebook_size), dtype=np.uint16)
     dimensions = len(data_set[0])
 
-    #3d view settings
-    elevation = 30
-    azimuth = 45
-
-    # generate codebook vectors
+    # Generate codebook vectors
     codebook = np.array([random_vector([(0, 1) for k in xrange(dimensions)]) for i in xrange(codebook_size)])
 
     prevDone = 0
@@ -98,14 +112,14 @@ def TRN(data_set, max_iterations, codebook_size, epsilon_i, epsilon_f, lambda_i,
         # Select random data point
         random_data_point = data_set[random.randint(0, len(data_set)-1)]
 
-        # for each w[i] find the number of w[j] such that || x-w[j] || < || x - w[i] ||
+        # For each w[i] find the number of w[j] such that || x-w[j] || < || x - w[i] ||
         # array of all x - w[i]
         # array of all || x - w[i] ||
         # using numpy vectorization
         V = random_data_point - codebook
         distances = np.sqrt(np.multiply(V, V)).sum(axis=1)
 
-        # update the codebook vectors according to the neural gas algorithm
+        # Update the codebook vectors according to the neural gas algorithm
         lambda_val = lambda_i * ((lambda_f / lambda_i) ** iter_fraction);
         epsilon = epsilon_i * ((epsilon_f / epsilon_i) ** iter_fraction);
 
@@ -114,14 +128,14 @@ def TRN(data_set, max_iterations, codebook_size, epsilon_i, epsilon_f, lambda_i,
         coefficients = epsilon * codebooks_near
         codebook += coefficients.reshape((len(coefficients),1)) * V
 
-        # find closest two codebook indices
+        # Find closest two codebook indices
         smallest1, smallest2 = heapq.nsmallest(2, [(v, i) for (i, v) in enumerate(distances)])
         index_smallest1, index_smallest2 = smallest1[1], smallest2[1]
 
-        # create connection and refresh existing connection
+        # Create connection and refresh existing connection
         connections[index_smallest1, index_smallest2] = 1
 
-        # age all connections
+        # Age all connections
         max_age = T_i * ((T_f / T_i) ** iter_fraction)
         np.putmask(connections, connections != 0, connections + 1) # Age all non-zero entries (zero => no connection)
         np.putmask(connections, connections > max_age, 0) # Remove connections greater than max age
@@ -146,7 +160,7 @@ def draw_graph_3d(G, positions, dataset, file_name, el, az):
     plt.figure.max_num_figures = 20
     ax = fig.add_subplot(111, projection='3d')
 
-    #draw lines between codebook vectors
+    # Draw lines between codebook vectors
     lines = [[positions[x],positions[y]] for (x,y) in G.edges()]
     for line in lines:
         x,y,z = zip(*line)
@@ -197,25 +211,19 @@ def main(fileName, codebookSize):
     print "Connecting unconnected edges..."
     connections = connect_graph(codebook, connections)
     print ""
-    print "TRN Runtime:", time.time() - t0, "seconds"
-
     print "Scaling down to 2 dimensions..."
 
     # Scale codebook down to two dimensions using MDS
     scaled_codebook = MDS(codebook, 2)
+    print "TRNMap Runtime:", time.time() - t0, "seconds"
 
     print "Drawing graph..."
     N, scaled_positions = connections_to_graph(scaled_codebook, connections)
-    nx.draw(N, pos=scaled_positions, node_color="#BEF202", dim=2)
-    plt.savefig("graph_2d.jpg", filetype="jpg")
-    #for i in range(0,360):
-    #    draw_graph_3d(M, positions, dataset, folder_path+"/graph_3d_"+str(i)+".jpg", 30, i)
+    #nx.draw(N, pos=scaled_positions, node_color="#BEF202", dim=2)
+    #plt.savefig("graph_2d.jpg", filetype="jpg")
 
-    #print "Number of subgraphs:", nx.number_connected_components(M)
-    #print M.nodes()
-    #print M.edges()
+    # Output graph to JSON that Sigma/D3.js can read
     output_json(N, "graph.json")
-    #output_gexf(M)
     print "Done!"
 
 if __name__ == "__main__":
